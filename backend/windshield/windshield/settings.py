ce86dc25c0c9458199551bdf5d1395a6ebf24a4c
62090500409 <thanaphit.su@mail.kmutt.ac.th>
2022-05-08 23:14:51 +0700

    Update Admin Backend Site

--- backend/windshield/windshield/settings.py
+++ backend/windshield/windshield/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_numeric_filter',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
